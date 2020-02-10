90aedae0b7b93e39468da351e45b0a5998c1fcb0
Diego Capassi <diego.capassi.moreira@gmail.com>
2020-02-09 22:38:17 -0200

    Added input validation to the SessionController using the yup library

--- routes.js
+++ routes.js
@@ -1,7 +1,12 @@
 import { Router } from 'express';
+import User from './src/app/models/User';
+import SessionController from './src/app/controllers/SessionControllers';
+
+import authMiddlware from './src/app/middlewares/auth';
 
 const routes = new Router();
 
 routes.get('/', (req, res) => res.send({ message: 'Hello World' }));
+routes.post('/sessions', SessionController.store);
 
 export default routes;
